1e0d0b3011f0c0957b5d77521ca0177c43e732db
nikitawong19 <iamspecial19@gmail.com>
2024-05-11 23:38:58 +0800

    Updated

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -27,4 +27,4 @@ async function getInventoryByClassificationId(classification_id) {
 
 module.exports = {getClassifications, getInventoryByClassificationId};
 
-module.exports = {getClassifications}
+// module.exports = {getClassifications}
